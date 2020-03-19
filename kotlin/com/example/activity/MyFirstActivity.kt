ac6982b6a2000c650e9a4bd18a59e3f3ba13ada6
Jinlin Zhang <jinlinzhang@fb.com>
2020-03-19 16:18:04 -0700

    Android Ktolin Sample

---
+++ kotlin/com/example/activity/MyFirstActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+
+class MyFirstActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
